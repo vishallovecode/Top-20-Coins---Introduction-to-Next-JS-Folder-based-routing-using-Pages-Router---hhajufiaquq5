c4137eb6799f6da80302ad00e9c5686df97ab8b1
Vishal Sharma <785vishal443@gmail.com>
2023-10-30 21:31:01 +0530

    Update CoinCard.js

--- app/components/CoinCard.js
+++ app/components/CoinCard.js
@@ -1,17 +1,19 @@
 import React from "react";
-import { Link } from "next/link";
+import Link from "next/link";
 
 function CoinCard({ coin }) {
-
-    return (
-        <div className="coin-card">
-            <h3 className="coin-name">coin-name</h3>
-            <h4 className="coin-symbol">coin-symbol</h4>
-            <p className="coin-rank">Rank: coin-symbol</p>
-            <p className="coin-price">Price: $coin-price</p>
-            <Link className="view-details-link" href="">View Details</Link>
-        </div>
-    )
+  // const [coin] =props;
+  return (
+    <div className="coin-card">
+      <h3 className="coin-name">{coin.name}</h3>
+      <h4 className="coin-symbol">{coin.symbol}</h4>
+      <p className="coin-rank">Rank: {coin.rank}</p>
+      <p className="coin-price">Price: ${coin.price_usd}</p>
+      <Link href={`/${coin.id}`} className="view-details-link">
+        View Details
+      </Link>
+    </div>
+  );
 }
 
-export default CoinCard
+export default CoinCard;
