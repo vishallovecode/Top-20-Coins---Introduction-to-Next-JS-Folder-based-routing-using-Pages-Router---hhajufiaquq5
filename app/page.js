b5b875125996c2cd7daa7469bde2e9e705d9e9ac
Vishal Sharma <785vishal443@gmail.com>
2023-10-30 21:42:36 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import CoinCard from "./components/CoinCard";
+import CoinCard from "./CoinCard";
 
 function Home() {
   const [coins, setCoins] = useState([]);
