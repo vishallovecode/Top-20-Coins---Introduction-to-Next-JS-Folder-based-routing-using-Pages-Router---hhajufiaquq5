aaf5ee908c3c99e09fee56d169307a649695ec86
Vishal Sharma <785vishal443@gmail.com>
2023-10-30 21:31:41 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,20 +1,31 @@
-'use client'
-import React, { useState, useEffect } from 'react';
-import CoinCard from './components/CoinCard';
+"use client";
+import React, { useState, useEffect } from "react";
+import CoinCard from "./components/CoinCard";
 
 function Home() {
-    const [coins, setCoins] = useState([]);
+  const [coins, setCoins] = useState([]);
 
-    return (
-        <div className='home'>
-            <h1>Top 20 Cryptos</h1>
-            <div className='coins-container'>
-                {coins.map((coin) => (
-                    <CoinCard />
-                ))}
-            </div>
-        </div>
-    )
+  const fetchCoins = async () => {
+    const response = await fetch("https://api.coinlore.net/api/tickers/");
+    const result = await response.json();
+    const firstTwentyCoins = result.data.slice(0, 20);
+    setCoins(firstTwentyCoins);
+  };
+
+  useEffect(() => {
+    fetchCoins();
+  }, []);
+
+  return (
+    <div className="home">
+      <h1>Top 20 Cryptos</h1>
+      <div className="coins-container">
+        {coins.map((coin) => (
+          <CoinCard key={coin.id} coin={coin} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
