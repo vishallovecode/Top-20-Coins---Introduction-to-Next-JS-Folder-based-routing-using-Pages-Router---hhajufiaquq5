87c93a3906176370b22689b066581e1e6f1a5b02
Vishal Sharma <785vishal443@gmail.com>
2023-10-30 21:42:17 +0530

    Create CoinCard.js

---
+++ app/CoinCard.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Link  from "next/link";
+
+function CoinCard({ coin }) {
+
+    return (
+        <div className="coin-card">
+            <h3 className="coin-name">{coin.name}</h3>
+            <h4 className="coin-symbol">{coin.symbol}</h4>
+            <p className="coin-rank">Rank: {coin.rank}</p>
+            <p className="coin-price">Price: ${coin.price_usd}</p>
+            <Link href={`/${coin.id}`} className="view-details-link">View Details</Link>
+        </div>
+    )
+}
+
+export default CoinCard
